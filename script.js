869268a8c128c119fe3376fffa84d2f0d5a6b38e
indianboomi <indianboomi157@gmail.com>
2025-08-14 13:42:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
-
-    let lang = localStorage.getItem("selectedLang");
-    if (lang) {
-        setTimeout(() => {
-            let select = document.querySelector(".goog-te-combo");
-            if (select) {
-                select.value = lang;
-                select.dispatchEvent(new Event("change"));
-            }
-        }, 800);
-    }
-}
+document.getElementById('playBtn').addEventListener('click', function() {
+    alert("Starting Minecraft server...");
+    // Here you can redirect to your server page or do something else
+});
